e13871bc3985629c902bfc05bada52b95733ff01
Emil Johansson <emijohansson@deloitte.com>
2015-02-11 19:57:16 -0700

    change spec template to use mocha

--- templateSpec.js
+++ templateSpec.js
@@ -1,10 +1,11 @@
+var assert = require("assert");
 describe('File Name', function() {
     'use strict';
 
     var factoryMethod = require('./{fileName}');
 
     it("should exist", function() {
-        expect(typeof factoryMethod).toBe('function');
-        expect(typeof factoryMethod()).toBe('object');
+        assert.equal(typeof factoryMethod, 'function');
+        assert.equal(typeof factoryMethod(), 'object');
     });
 });
