df0e8fc3c1f079f84c69bccf29b4b1afeb8f72ff
Emil Johansson <emiljohansson.se@gmail.com>
2015-04-12 09:28:34 -0600

    remove logs

--- angular/index.js
+++ angular/index.js
@@ -1,18 +1,9 @@
 "use strict";
 
-var fs = require('fs');
 var gen = require('./generators');
 var argv = require('minimist')(process.argv.slice(2));
 var availableGenerators = gen.find(argv);
-
-console.dir(argv);
-console.dir(availableGenerators);
-console.dir(availableGenerators.length);
-
 if (availableGenerators.length < 1) {
     return;
 }
-
 require(availableGenerators[0].path);
-
-console.log('file generated');
